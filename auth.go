6a030b96afb70c7430ffa7d3663233dd6d9c08bf
Luciano Zangeronimo <zangeronimo@gmail.com>
2020-06-25 18:58:09 -0300

    Refactored structure and created a Login func

--- auth.go
+++ auth.go
@@ -1,31 +1,65 @@
 package auth
 
 import (
+	"encoding/base64"
 	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
-//Hello is a basic function for test
-func Hello() string {
-	return "Hello world."
+var basicUser = ""
+var basicPass = ""
+
+//SetBasicUser add a basic username to validade a basic login
+func SetBasicUser(bu string) {
+	basicUser = bu
 }
 
-//BasicRequest populate this struct with BasicAuth username and password geted in client request
-type BasicRequest struct {
-	ReqUsername string
-	ReqPassword string
+//SetBasicPass add a basic password to validade a basic login
+func SetBasicPass(bp string) {
+	basicPass = bp
 }
 
-//BasicLocal create this struct with Basic username and password requested in your project
-type BasicLocal struct {
-	LocUsername string
-	LocPassword string
-}
+//basicAuth authentication with basic login, use this to validate a request for a new JWT
+func basicAuth(r *http.Request) error {
+	// Check basic login for basic authentication, first stap to generate a JWT
+	auth := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
 
-//BasicAuth authentication with basic login, use this to validate a request for a new JWT
-func BasicAuth(basicRequest BasicRequest, basicLocal BasicLocal) (bool, error) {
-	if (basicRequest.ReqUsername == basicLocal.LocUsername) &&
-		(basicRequest.ReqPassword == basicLocal.LocPassword) {
-		return true, nil
+	if len(auth) != 2 || auth[0] != "Basic" {
+		return fmt.Errorf("authorization failed")
 	}
-	return false, fmt.Errorf("authorization failed")
+
+	payload, _ := base64.StdEncoding.DecodeString(auth[1])
+	pair := strings.SplitN(string(payload), ":", 2)
+
+	if len(pair) == 2 && pair[0] == basicUser && pair[1] == basicPass {
+		return nil
+	}
+	return fmt.Errorf("authorization failed")
+}
+
+//Login Use this func to authenticate a user and return a valid jwt
+func Login(w http.ResponseWriter, r *http.Request) {
+	err := basicAuth(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	// Create a new token object, specifying signing method and the claims
+	// you would like it to contain.
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"foo": "bar",
+		"nbf": time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(os.Getenv("SAMPLESECRET")))
+
+	fmt.Println(tokenString, err)
+	fmt.Fprintf(w, "Hello, %s - Err %s!", tokenString, err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zangeronimo/auth
 
 go 1.14
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
